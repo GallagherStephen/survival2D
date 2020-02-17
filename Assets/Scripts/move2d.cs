8e5b209ad1d0db84ec89a12282142f05f2494383
Stephen gallagher <stephen.gallagherit3@gmail.com>
2020-02-17 17:15:49 +0000

    added in an enemy + bullet need to do both scripts

--- Assets/Scripts/move2d.cs
+++ Assets/Scripts/move2d.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class move2d : MonoBehaviour
 {
-
+    public Transform player;
     public float moveSpeed = 5f;
+    public GameObject bulletPrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +20,10 @@ public class move2d : MonoBehaviour
         Jump();
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += movement * Time.deltaTime * moveSpeed;
+        if (Input.GetMouseButton(0)) // 0 being left button?? not working
+        {
+            shootBullet(); //shoot
+        }
     }
 
     void Jump()
@@ -27,6 +32,12 @@ public class move2d : MonoBehaviour
         {
             gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse);
         }
+    }
+
+    public void shootBullet()
+    {
+        GameObject b = Instantiate(bulletPrefab) as GameObject;
+        b.transform.position = player.transform.position; //keeps the bullt location stuck to the player position
     }
 
 }
