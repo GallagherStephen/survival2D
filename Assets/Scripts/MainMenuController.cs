45b49ec5e439cbed6334b783cf699065e4f7f73c
Stephen gallagher <stephen.gallagherit3@gmail.com>
2020-02-18 12:50:23 +0000

    added menu+scripts, still need player+ enemy scripts better

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//add this for scene management for unity
+using UnityEngine.SceneManagement;
+
+
+public class MainMenuController : MonoBehaviour
+{
+    //handle button events
+    // start button, options button 
+    //use scenemanager to load and unload scenes
+    //all methods are static , subscribe to events
+    //like sceneloaded,sceneUnloaded,SceneChanged for custon logic
+    //use LoadSceneAsync to load a new scene
+
+    //SceneManager is in unityEngine.SceneManagement library
+
+
+    // == OnClick events ==
+
+public void Start_OnClick()
+    {
+        SceneManager.LoadSceneAsync("Game"); //passing in the name of the scene as displayed in unity
+    }
+
+    public void Options_OnClick()
+    {
+        SceneManager.LoadSceneAsync("Options Menu", LoadSceneMode.Additive); //passing in the name of the scene as displayed in unity as an overlay to game not a new scene entirely
+    }
+}
---
+++ Assets/Scripts/Optionscontroller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//add this for scene management for unity
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    
+//== on_click events ==
+
+public void Back_OnClick()
+    {
+        SceneManager.UnloadSceneAsync("Options menu");
+        
+    }
+
+
+
+
+}
