e76c0d493321ea81a08e98682432171825aa9036
Stephen gallagher <stephen.gallagherit3@gmail.com>
2020-03-03 13:45:56 +0000

    got enemy to move across screen

--- Assets/Scripts/enemyScript.cs
+++ Assets/Scripts/enemyScript.cs
@@ -8,6 +8,10 @@ public class enemyScript : MonoBehaviour
     public int health = 100; //enemy health
     public GameObject deathEffect;
 
+    
+    public float speed;
+    public bool MoveLeft;
+
     public void TakeDamage(int damage) //takes in how much damage to take 
     {
         health = health - damage; //subtract from our health
@@ -24,5 +28,20 @@ public class enemyScript : MonoBehaviour
         Instantiate(deathEffect, transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
+
+    void Update()
+    {
+        if (MoveLeft)
+        {
+            transform.Translate(2 * Time.deltaTime * speed, 0,0);
+        }
+        else //move right(-2)
+        {
+            transform.Translate(-2 * Time.deltaTime * speed, 0,0);
+
+        }
+    }
+
+
 
 }
