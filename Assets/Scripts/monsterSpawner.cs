80fdde4e12c5af791ad6e44adc983e3ea3409377
Stephen gallagher <stephen.gallagherit3@gmail.com>
2020-03-03 14:13:24 +0000

    enemy spawnPoints, able to select the amnt of spawns + enemies

---
+++ Assets/Scripts/monsterSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class monsterSpawner : MonoBehaviour
+{
+
+    public Transform[] spawnpoints; //creating a spawnpoints array
+    public GameObject[] monsters; //creating a monster array
+    int randomSpawnPoint, randomMonster;
+    public static bool spawnAllowed;
+    
+    void Start()
+    {
+        spawnAllowed = true;
+        InvokeRepeating("SpawnAMonster", 0f, 1f); //spawn a monster every 1 second
+    }
+
+    // Update is called once per frame
+    void SpawnAMonster()
+    {
+        randomSpawnPoint = Random.Range(0, spawnpoints.Length);
+        randomMonster = Random.Range(0, monsters.Length);
+        Instantiate(monsters[randomMonster], spawnpoints[randomSpawnPoint].position, Quaternion.identity);
+    }
+}
