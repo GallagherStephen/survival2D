9d117f2afb73923b944f0801ed702c9d192ee2c9
Stephen gallagher <stephen.gallagherit3@gmail.com>
2020-04-16 15:50:52 +0100

    added that the score resets to 0 when the level1 game scene is loaded

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -7,6 +7,10 @@ public class ButtonManager : MonoBehaviour
 {
    public void ButtonMoveScene(string level)
     {
+        if(level == "Game")
+        {
+            scorescript.scoreValue = 0; //sets the score back to 0 when level1/game scene is called
+        }
         SceneManager.LoadScene(level); //load the level we select on unity screen
     }
 }
